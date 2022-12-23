9cdc33a701365f94b4675cd3391b8a2b15ccdccf
Harika Rachamalla <harika.0293@gmail.com>
2022-12-23 18:37:34 +0530

    Updates

---
+++ precache-manifest.6b85d8b6c92d3a99ea2eb2e45d6b0103.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8b840a8fae264ce21b685c264a1062",
+    "url": "/React-JS-E-Commerce/index.html"
+  },
+  {
+    "revision": "3e3f6dfd5856250d936a",
+    "url": "/React-JS-E-Commerce/static/js/2.692393f1.chunk.js"
+  },
+  {
+    "revision": "a712f5023b98f1ee6f879156125341ec",
+    "url": "/React-JS-E-Commerce/static/js/2.692393f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08faa9b0928e15232511",
+    "url": "/React-JS-E-Commerce/static/js/main.2858b6f7.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/React-JS-E-Commerce/static/js/main.2858b6f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "200c180287d2a8acb824",
+    "url": "/React-JS-E-Commerce/static/js/runtime-main.586ba668.js"
+  },
+  {
+    "revision": "422fec1079cce450353e924288f8313b",
+    "url": "/React-JS-E-Commerce/static/media/commerce.422fec10.png"
+  }
+]);
